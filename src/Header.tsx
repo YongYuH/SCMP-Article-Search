b16e487ee926c3cf6fa9edff8fa598000dcbca01
YongYuH <larry830326@hotmail.com>
2021-08-24 19:53:17 +0800

    feat: add header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,25 +1,41 @@
 import styled from '@emotion/styled'
 import React from 'react'
+import { BsList } from 'react-icons/bs'
 
 import ScmpLogoIcon from './scmp_logo.svg'
 
-const logoImageHeight = 40
-const logoImageWidth = 40
+const logoImageHeight = 30
+const logoImageWidth = 30
 
+const Wrapper = styled.div`
+  display: grid;
+  align-items: center;
+  grid-auto-flow: column;
+  grid-column-gap: 8px;
+  grid-template-columns: max-content;
+`
 const LogoWrapper = styled.div`
   height: ${logoImageHeight}px;
   width: ${logoImageWidth}px;
 `
+const Title = styled.div``
+const ListIconWrapper = styled.div`
+  justify-self: flex-end;
+`
 
 const Header = () => (
-  <div>
+  <Wrapper>
     <LogoWrapper>
       <ScmpLogoIcon
         height={`${logoImageHeight}px`}
         width={`${logoImageWidth}px`}
       />
     </LogoWrapper>
-  </div>
+    <Title>South China Morning Post</Title>
+    <ListIconWrapper>
+      <BsList />
+    </ListIconWrapper>
+  </Wrapper>
 )
 
 export default Header
